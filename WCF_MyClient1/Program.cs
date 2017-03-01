c201fa234fbda986597f4a29d3ac9d5fdb6962ea
user1 <tyaa@ukr.net>
2017-03-01 11:08:37 +0200

    Добавьте файлы проекта.

---
+++ WCF_MyClient1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WCF_MyClient1
+{
+    //Модель данных для отправки серверной службе
+    public class PersonFromClient
+    {
+        public Int32 Age { get; set; }
+        public String Name { get; set; }
+    }
+    //Контракт для передачи данных серверной службе 
+    [ServiceContract]
+    public interface IService2
+    {
+        [OperationContract]
+        int addPerson(XElement _personXElement);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Именно такой тип привязки необходим для работы со службами, размещенными в ASP.NET-приложении
+            BasicHttpBinding binding = new BasicHttpBinding();
+            //Для фабрики указываем адрес, который можно узнать, запустив серверную службу с тестовым клиентом
+            ChannelFactory<IService2> factory = new ChannelFactory<IService2>(
+                binding,
+                new EndpointAddress("http://localhost:10561/Service2.svc"));
+            //Создаем канал для работы с серверной службой
+            IService2 channel = factory.CreateChannel();
+            //Создаем пустой объект контракта данных и инициализируем его поля
+            PersonFromClient personFromClient = new PersonFromClient();
+            personFromClient.Age = 30;
+            personFromClient.Name = "Petia";
+            //Создаем сериализатор
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(PersonFromClient));
+            //Создаем объект XDocument и сериализуем в него объект с данными
+            XDocument xDocument = new XDocument();
+            using (XmlWriter xmlWriter = xDocument.CreateWriter())
+            {
+                xmlSerializer.Serialize(xmlWriter, personFromClient);
+            }
+            //Сохраняем сериализованные данные в объект XElement, подходящий для отправки серверной службе
+            XElement personXElement = xDocument.Root;
+            //Передаем серверной службе данные и ожидаем в ответ значение 0
+            int result = channel.addPerson(personXElement);
+            //Выводи в консоль ответ сервера, подтверждающий, что операция выполнена
+            Console.WriteLine("result: {0}", result);
+            Console.WriteLine("Для завершения нажмите <ENTER>\n");
+            Console.ReadLine();
+            factory.Close();
+        }
+    }
+}
---
+++ Wpf_client/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Wpf_client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    //Модель данных для отправки серверной службе
+    public class PersonFromClient
+    {
+        public Int32 Age { get; set; }
+        public String Name { get; set; }
+    }
+    //Контракт для передачи данных серверной службе 
+    [ServiceContract]
+    public interface IService2
+    {
+        [OperationContract]
+        int addPerson(XElement _personXElement);
+    }
+    public partial class MainWindow : Window
+    {
+        Color leaveGrid;
+        public MainWindow()
+        {
+            InitializeComponent();
+            leaveGrid = Color.FromArgb(100, 0, 0, 100);
+            MyWindow.Background = new SolidColorBrush(leaveGrid);
+        }
+
+        private void MyWindow_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        Point p, p_, p__;
+        bool flag02 = true, flag;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == buttonCancel)
+                this.Close();
+            else if (sender == buttonLoading)
+            {
+                //MessageBox.Show(proxy.GetDataTranslate().Count()+"");
+                //foreach (Alfa.DataTranslate item in proxy.GetDataTranslate())
+                //{
+                //    DataTranslate obj = new DataTranslate();
+                //    obj.Word = item.Word;
+                //    obj.Description = item.Description;
+                //    //MessageBox.Show(item.Word+ item.Description);
+                //    Mas.dictionary.Add(obj);
+                //}
+                ChannelFactory<IService2> factory = null;
+                try
+                {
+                    BasicHttpBinding binding = new BasicHttpBinding();
+                    factory = new ChannelFactory<IService2>(
+                        binding,
+                        new EndpointAddress("http://localhost:10561/Service2.svc"));
+                    IService2 channel = factory.CreateChannel();
+                    PersonFromClient personFromClient = new PersonFromClient();
+                    personFromClient.Name = textBoxName.Text;
+                    personFromClient.Age = Int32.Parse(textBoxAge.Text);
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(PersonFromClient));
+                    XDocument xDocument = new XDocument();
+                    using (XmlWriter xmlWriter = xDocument.CreateWriter())
+                    {
+                        xmlSerializer.Serialize(xmlWriter, personFromClient);
+                    }
+                    XElement personXElement = xDocument.Root;
+                    int result = channel.addPerson(personXElement);
+                    textBlockStatus.Text = "Данные загружены на сервер успешно";
+                }
+                catch (Exception)
+                {
+                    textBlockStatus.Text = "Загрузка данных не удалась";
+                }
+                finally
+                {
+                    factory.Close();
+                }
+            }
+        }
+
+        private void Text_Changed(object sender, TextChangedEventArgs e)
+        {
+            textBlockStatus.Text = "Element";
+        }
+
+        private void MyWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            flag = true;
+        }
+
+        private void MyWindow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            flag = false;
+            flag02 = true;
+        }
+
+        private void MyWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flag)
+            {
+                if (flag02)
+                {
+                    p__ = e.GetPosition(this);
+                    flag02 = false;
+                }
+                p = e.GetPosition(this);
+                p_ = PointToScreen(p);
+                Top = p_.Y - p__.Y;
+                Left = p_.X - p__.X;
+            }
+        }
+    }
+}
